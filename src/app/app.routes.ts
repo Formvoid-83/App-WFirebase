c795378cad60cf6feef00ac42c12b6e3fb0b34dd
Formvoid-83 <guillermomolinaggg@gmail.com>
2024-03-22 01:29:26 -0400

    First commit of the practice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,20 @@ export const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./pages/signup/signup.page').then( m => m.SignupPage)
+  },
+  {
+    path: 'reset-password',
+    loadComponent: () => import('./pages/reset-password/reset-password.page').then( m => m.ResetPasswordPage)
+  },
+  {
+    path: 'landing',
+    loadComponent: () => import('./landing/landing.page').then( m => m.LandingPage)
+  },
 ];
