8c424c3a40beeecd4716dd29522033c303cf1a1e
GuillermoM-83 <guillermomolinaggg@gmail.com>
2024-04-15 23:31:56 -0400

    New position and appearance for Sign/Login

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,13 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { personOutline } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-nav-bar',
   standalone:true,
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
+  imports: [IonicModule, CommonModule, HttpClientModule, FormsModule]
 })
 export class NavBarComponent {
 
-  constructor() { }
+  constructor() {
+    addIcons({personOutline});
+   }
 
 }
